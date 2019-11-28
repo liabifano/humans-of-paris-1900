3802a89a93d7bdeda5b0ba285fdec22e1ffead18
liabifano <liamarciavbifano@gmail.com>
2019-11-28 11:09:41 +0100

    autocomplete for + standard for every page

--- humans_of_paris/app/urls.py
+++ humans_of_paris/app/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('cluster', views.cluster, name='cluster'),
+    path('facemap', views.cluster, name='facemap'),
     path('yourdoppelganger', views.yourdoppelganger, name='yourdoppelganger'),
     path('record/<str:id>/', views.record, name='record'),
     path('person_records/<str:name>/', views.person_records, name='person_records'),
