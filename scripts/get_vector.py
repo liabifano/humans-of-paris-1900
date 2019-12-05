fcd9ed21de5939241fae5970a0e8c300f53995a4
Leonore Guillain <leonore.guillain@epfl.ch>
2019-12-05 10:27:36 +0100

    fix openface

--- scripts/get_vector.py
+++ scripts/get_vector.py
@@ -1,13 +1,17 @@
 import argparse
 import cv2
+from io import BytesIO
+from PIL import Image
 import itertools
 import os
-import pandas as pd
 
 import numpy as np
 np.set_printoptions(precision=2)
-
+import pandas as pd
+import requests
 import openface
+import json
+
 
 
 def get_image(doc):
@@ -19,7 +23,7 @@ def get_image(doc):
     img = np.array(img)
     return img
 
-def getRep(imgPath):
+def getRep(imgPath, imgDim=96):
 
     bgrImg = get_image(imgPath)
     if bgrImg is None:
@@ -30,7 +34,7 @@ def getRep(imgPath):
     if bb is None:
         raise Exception("Unable to find a face: {}".format(imgPath))
 
-    alignedFace = align.align(args.imgDim, rgbImg, bb,
+    alignedFace = align.align(imgDim, rgbImg, bb,
                               landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
     if alignedFace is None:
         raise Exception("Unable to align image: {}".format(imgPath))
@@ -40,9 +44,32 @@ def getRep(imgPath):
 
 
 if __name__ == '__main__':
-    sample = pd.read_csv('identifiers.csv', header=None)
+
+    modelDir = 'models'
+    print(modelDir)
+    dlibModelDir = 'models/dlib'
+    print(dlibModelDir)
+    openfaceModelDir = 'models/openface'
+
+
+    dlibFacePredictor = os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat")
+    align = openface.AlignDlib(dlibFacePredictor)
+
+    networkModel = os.path.join(openfaceModelDir, 'nn4.small2.v1.t7')
+    imgDim = 96
+    net = openface.TorchNeuralNet(networkModel, imgDim)
+
+    sample =  pd.read_json('age_gender_labeles.json').T
     faces = []
-    for doc in sample[1].sample(2):
-        face = getRep(doc)
-        faces.append(face)
-        np.save('faces.npy', faces)
+    for doc in sample.id:
+
+        try:
+            print('INFO : parsing ', doc)
+            face = getRep(doc, imgDim)
+            faces.append({'id': doc, 'face': list(face)})
+        except:
+            print('Failed on ', doc)
+            continue
+
+        with open('faces.json', 'w') as f:
+            json.dump(faces, f)
