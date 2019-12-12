7cba172283b9ff76dad543420e985556df8eb1fb
liabifano <liamarciavbifano@gmail.com>
2019-12-12 14:42:04 +0100

    new page for tags

--- humans_of_paris/app/urls.py
+++ humans_of_paris/app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('facemap', views.cluster, name='facemap'),
     path('yourdoppelganger', views.yourdoppelganger, name='yourdoppelganger'),
     path('person/<str:name>/', views.person, name='person'),
+    path('tag/<str:tag>/', views.tag, name='tag')
 ]
